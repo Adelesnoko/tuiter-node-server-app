2419f6cb490bc4ada88c654ed7901d2247534fd9
Adelesnoko <adela_feng@outlook.com>
2023-06-12 23:15:51 -0700

    updated a5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // const express = require('express')
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import HelloController from './controllers/hello-controller.js';
 import UserController from './users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
